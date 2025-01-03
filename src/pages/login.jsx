5a4027a8e99043542380187f8c18e4a488711a48
Oknown15 <buchijunior90@gmail.com>
2025-01-03 23:52:46 +0100

    tests

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,11 +3,10 @@
 // import { Link } from 'react-router-dom';
 import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Loader from "../component/loginPreloader";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../firebase';
-
 function Login() {
     const [ email, setEmail] = useState("")
     const [password, setPassword] = useState("")
@@ -54,9 +53,14 @@ function Login() {
                     </div>
                     <TextInput onChange={(e) => setPassword(e.target.value)} id="password1" type="password" required />
                 </div>
-                <div className="flex items-center gap-2">
-                    <Checkbox id="remember" />
-                    <Label htmlFor="remember">Remember me</Label>
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                        <Checkbox id="remember" />
+                        <Label htmlFor="remember">Remember me</Label>
+                    </div>
+                    <div>
+                        <Link className="text-black hover:text-blue-800" to="/register">Register</Link>
+                    </div>
                 </div>
                 <p className="m-0 text-red-800">{ error }</p>
                 <Button type="submit">{ loading ? <Loader/> : "Submit" }</Button>
